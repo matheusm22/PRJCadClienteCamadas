fe46d45333f4d667ee122f731f5ac8c096687ab9
matheusm22 <matheus.mottademello@gmail.com>
2019-05-14 15:24:36 -0300

    Criação do pacote Persistencia e da classe CRUDCliente

---
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -0,0 +1,44 @@
+package com.prjcadcliente.persistencia;
+
+import java.util.List;
+
+import com.prjcadcliente.dominio.Cliente;
+
+/**
+ *<b>CRUDCliente</b>
+ * Essa classe permite manipular as informações do cliente. Aqui você 
+ * encontrará os seguintes comandos:
+ * <ul>
+ * <Li>Cadastro</Li>
+ * <Li>Pesquisar por nome e por id</Li>
+ * <Li>Atualizar</Li>
+ * <Li>Deletar</Li>
+ * @author matheus.mmello
+ *
+ */
+public class CRUDCliente {
+	public String cadastrar(Cliente cliente) {
+		return null;
+	}
+	
+	public String atualizar(Cliente cliente) {
+        return null;
+        
+	}
+	public String deletar(Cliente cliente) {
+        return null;
+
+	}
+	
+	public List<Cliente> PesquisarPorNome(String nome){
+		return null;
+	}
+	public Cliente PesquisarPorID(int id){
+		return null;
+	}
+	
+	
+	public List<Cliente> PesquisarTodos(){
+		return null;
+	}
+}
